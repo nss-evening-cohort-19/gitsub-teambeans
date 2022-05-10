efed8d0439a3208f34c2f16244b4956be0ac043e
nishaya raj <nishayaraj@gmail.com>
2022-05-09 20:54:38 -0500

    Added navbar function in js

---
+++ navbar.js
@@ -0,0 +1,34 @@
+const setInnerHtml = (id, htmlString) =>
+  (document.getElementById(id).innerHTML = htmlString);
+
+
+const displayNavBar = () => {
+  let domString = "";
+  domString = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Overview</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"> Repositories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Packages</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>`;
+
+  setInnerHtml("navBar", domString);
+};
+
+displayNavBar();
