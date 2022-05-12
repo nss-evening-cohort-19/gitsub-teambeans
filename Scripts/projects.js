46926d412d2a5f5b5ab7655131444b030e17be0e
nishaya raj <nishayaraj@gmail.com>
2022-05-11 19:30:28 -0500

    add project function,form display and list display

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -4,40 +4,58 @@ import { projectData } from "./fakedata.js";
 //change this to forEach loop
 const renderProjectsList = () => {
   let domString = "";
-  domString = `<div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action">
-  <div class="d-flex w-100 justify-content-between">
-    <h5 class="mb-1">"${projectData.name}"</h5>
-    <small class="text-muted">"${projectData.updateTime}"</small>
-  </div>
-  <p class="mb-1">"${projectData.description}"</p>
-  <small class="text-muted">"${
-    projectData.private ? "private" : "public"
-  }"</small>
-</a>
-</div>`;
+  for (const project of projectData) {
+    domString+= `<div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">"${project.name}"</h5>
+      <small class="text-muted">"${project.updateTime}"</small>
+    </div>
+    <p class="mb-1">"${project.description}"</p>
+    <small class="text-muted">"${
+      project.private ? "private" : "public"
+    }"</small>
+  </a>
+  </div>`;
+  }
+  
 
   renderToDom("#mainContent", domString);
 };
 
 // //function is not added to main.js
+const addProject = (event) => {
+  event.preventDefault();
 
-// const projectForm = () => {
-//   let domString = "";
-//   domString = `<div class="mb-3">
-//   <label for="exampleFormControlInput1" class="form-label">Project Board name</label>
-//   <input type="email" class="form-control" id="exampleFormControlInput1">
-// </div>
-// <div class="mb-3">
-//   <label for="exampleFormControlTextarea1" class="form-label">Description(optional)</label>
-//   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-// </div>
-// <div class="col-12">
-//   <button type="submit" class="btn btn-primary">Create Project</button>
-// </div>
-// </div>`;
+  const newProject = {
+    id: document.querySelector().value,
+    name: document.querySelector().value,
+    updateTime: document.querySelector().value,
+    description: document.querySelector().value,
+    private: document.querySelector().value,
+  };
 
-//   renderToDom("#formContent", domString);
-// };
+  projectData.push(newProject);
+ renderProjectsList();
+};
+
+
+ const projectForm = () => {
+   let domString = "";
+   domString = `<div class="mb-3">
+   <label for="exampleFormControlInput1" class="form-label">Project Board name</label>
+   <input type="email" class="form-control" id="exampleFormControlInput1">
+ </div>
+ <div class="mb-3">
+   <label for="exampleFormControlTextarea1" class="form-label">Description(optional)</label>
+   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+ </div>
+ <div class="col-12">
+   <button type="submit" class="btn btn-primary" id="createProjectbtn">Create Project</button>
+ </div>
+ </div>`;
+
+   renderToDom("#formContent", domString);
+ };
 
 export { renderProjectsList };
